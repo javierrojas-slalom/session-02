69f0522ce77887fc27a8779448ea0911626e6c48
javierrojas-slalom <javier.rojassilva@slalom.com>
2026-07-21 01:00:01 +0000

    Revert "feat: e2e testing setup and implement TodoPage class"
    This reverts commit 96c8e11d64d191ae6cf003c7246d43d09a9a2e0b.

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
     trace: 'on-first-retry',
   },
   webServer: {
-    command: 'npm run start --prefix /workspaces/session-02',
+    command: 'npm run start',
     url: 'http://127.0.0.1:3000',
     timeout: 120000,
     reuseExistingServer: true,
